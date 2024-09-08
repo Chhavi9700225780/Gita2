12dc52bb7790af6f4764f79c9039028b4ff9915c
Chhavi9700225780 <c9081741@gmail.com>
2024-09-08 15:42:58 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -16,23 +16,9 @@ const verseSchema = require("./models/verseSchema");
 
 const PORT = process.env.PORT || 4000;
 
-const corsOptions = {
-  // origin: "http://localhost:5173",
-  origin: (origin, callback) => {
-    // Check if the origin is allowed
-    const allowedOrigins = [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://reliable-begonia-ccd2eb.netlify.app"
-    ];
-    const isAllowed = allowedOrigins.includes(origin);
-    callback(null, isAllowed ? origin : false);
-  },
-  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
-  credentials: true,
-};
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 
 
